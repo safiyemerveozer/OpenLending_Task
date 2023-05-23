29eda8095e3f6bc954573d52c4e1eb5ca834df43
Merve <safie.merveozer@gmail.com>
2023-05-23 18:30:55 -0500

    TestRunner ready

---
+++ src/test/java/com/openLending/runner/TestRunner.java
@@ -0,0 +1,21 @@
+package com.openLending.runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "html:target/cucumber-report.html",
+                "rerun:target/rerun.txt" ,
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+        },
+        features = "src/test/resources/features",
+        glue = "com/openLending/steps",
+        dryRun = true,
+        tags=""
+
+)
+public class TestRunner {
+}

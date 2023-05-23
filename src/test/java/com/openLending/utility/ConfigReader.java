795bdc54f73454506cf511ff011b5177a1c2f792
Merve <safie.merveozer@gmail.com>
2023-05-23 18:18:31 -0500

    Structure and Configuration Properties ready

---
+++ src/test/java/com/openLending/utility/ConfigReader.java
@@ -0,0 +1,22 @@
+package com.openLending.utility;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigReader {
+    private static Properties properties=new Properties();
+    static {
+        try {
+            FileInputStream file = new FileInputStream("config.properties");
+            properties.load(file);
+        }catch(IOException e){
+            System.out.println("File not found in ConfigReader");
+            e.printStackTrace();
+        }
+    }
+
+    public static String getProperty(String keyword){
+        return properties.getProperty(keyword);
+    }
+}

78e7516b6c096e8f71e4bab9e93eb52ccbf588c3
Merve <safie.merveozer@gmail.com>
2023-05-23 19:13:19 -0500

    us01StepDefinition update

--- src/test/java/com/openLending/steps/us01_StepDefinition.java
+++ src/test/java/com/openLending/steps/us01_StepDefinition.java
@@ -1,20 +1,27 @@
 package com.openLending.steps;
 
+import com.openLending.pages.GoogleSearchPage;
+import com.openLending.utility.ConfigReader;
+import com.openLending.utility.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.Keys;
 
 public class us01_StepDefinition {
     @Given("user on the Google page")
     public void user_on_the_google_page() {
-
+        Driver.getDriver().get(ConfigReader.getProperty("googleUrl"));
     }
+    GoogleSearchPage googleSearchPage=new GoogleSearchPage();
     @Given("user search for {string} in google search box")
     public void user_search_for_in_google_search_box(String searchValue) {
-
+        googleSearchPage.searchBar.sendKeys(searchValue+ Keys.ENTER);
     }
     @Given("user verify title is {string}")
     public void user_verify_title_is(String expectedTitle) {
-
+        String actualTitle=Driver.getDriver().getTitle();
+        Assert.assertEquals(expectedTitle,actualTitle);
     }
     @Given("user click Automated Lending Platform | Open Lending | United States image")
     public void user_click_automated_lending_platform_open_lending_united_states_image() {
